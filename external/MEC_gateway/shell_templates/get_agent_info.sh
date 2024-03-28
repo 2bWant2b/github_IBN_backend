8f7ba3e42e74c2008495445330b6b8e2761dc1bc
2bWant2b <tzh363231879@163.com>
2024-03-28 19:15:44 +0800

    test

--- external/MEC_gateway/shell_templates/get_agent_info.sh
+++ external/MEC_gateway/shell_templates/get_agent_info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #return: 网卡，连接方式，连接状态，连接名，ipv4地址
 echo "running $0"
-echo "kc304@KC304" | sudo nmcli device | awk 'NR>=2' | while read -r line
+sudo nmcli device | awk 'NR>=2' | while read -r line
 do
   net_card=($line)
   ipv4_address=$(nmcli d show ${net_card[0]} | awk '$1=="IP4.ADDRESS[1]:" {print $2}')
