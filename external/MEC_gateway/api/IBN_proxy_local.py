941d92b5f425cf44594fa69576903322e83d8960
2bWant2b <tzh363231879@163.com>
2024-03-28 19:13:32 +0800

    1

--- external/MEC_gateway/api/IBN_proxy_local.py
+++ external/MEC_gateway/api/IBN_proxy_local.py
@@ -6,7 +6,7 @@ def get_agent_info():
     # 执行Shell脚本
     shell_path = '../shell_templates/get_agent_info.sh'
     os.chmod(shell_path, 0o755)
-    result = subprocess.Popen(['sudo', shell_path], stdout=subprocess.PIPE)
+    result = subprocess.Popen([shell_path], stdout=subprocess.PIPE)
     # 获取脚本输出
     output, error = result.communicate()
     print(output.decode('utf-8'))
