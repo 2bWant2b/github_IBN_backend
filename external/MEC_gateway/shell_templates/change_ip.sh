bdc756256f59d99bc65daf7dbc6791fdd15a8b3b
2bWant2b <tzh363231879@163.com>
2024-03-28 20:00:57 +0800

    1

--- external/MEC_gateway/shell_templates/change_ip.sh
+++ external/MEC_gateway/shell_templates/change_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # arg1: 连接名称, arg2: ipv4地址
-echo "Running $0"
+# echo "Running $0"
 # nmcli connection modify test ipv4.address 192.168.168.30/24
 nmcli connection modify "$1" ipv4.address "$2"
 nmcli connection up "$1"
